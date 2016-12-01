4881df09c9f5ff0963f22d6aec5923fc92a10788
smileU <1484249625@qq.com>
2016-12-01 17:22:25 +0800

    Update mip-changyan.js

--- extensions/mip-changyan.js
+++ extensions/mip-changyan.js
@@ -1,40 +1,43 @@
 define("mip-changyan", ["require", "zepto", "customElement"], function(t) {
 	function n() {
 		var e = this.element;
-		if(!e.isRender) {
-			e.isRender = !0;
-			var appid = e.getAttribute("appid"),
-			conf = e.getAttribute("conf"),
-			a = i(e);
-			var width = window.innerWidth || document.documentElement.clientWidth; 
-			if (width < 960) { 
-				window.document.write('<script id="changyan_mobile_js" charset="utf-8" type="text/javascript" src="http://changyan.sohu.com/upload/mobile/wap-js/changyan_mobile.js?client_id=' + appid + '&conf=' + conf + '"><\/script>'); 
-			} else { 
-				var loadJs=function(d,a){
-					var c=document.getElementsByTagName("head")[0]||document.head||document.documentElement;
-					var b=document.createElement("script");
-					b.setAttribute("type","text/javascript");
-					b.setAttribute("charset","UTF-8");
-					b.setAttribute("src",d);
-					if(typeof a==="function"){
-						if(window.attachEvent){
-							b.onreadystatechange=function(){
-								var e=b.readyState;
-								if(e==="loaded"||e==="complete"){
-									b.onreadystatechange=null;
-									a()
-								}
-							}
-						}else{
-							b.onload=a
-						}
-					}c.appendChild(b)
-				};
-				loadJs("http://changyan.sohu.com/upload/changyan.js",function(){
-					window.changyan.api.config({appid:appid,conf:conf})
-				});
-			} 
-		}
+		var appid = e.getAttribute("appid"),
+		conf = e.getAttribute("conf"),
+		a = i(e);
+		var width = window.innerWidth || document.documentElement.clientWidth; 
+		if (width < 960) {
+            window.document.write('<script id="changyan_mobile_js" charset="utf-8" type="text/javascript" src="http://changyan.sohu.com/upload/mobile/wap-js/changyan_mobile.js?client_id=' + appid + '&conf=' + conf + '"><\/script>');
+        }
+        else {
+            var loadJs = function (srcStr, fn) {
+                var htmlTag = document.getElementsByTagName('head')[0] || document.head || document.documentElement;
+                var scriptTag = document.createElement('script');
+                scriptTag.setAttribute('type', 'text/javascript');
+                scriptTag.setAttribute('charset', 'UTF-8');
+                scriptTag.setAttribute('src', srcStr);
+                if (typeof fn === 'function') {
+                    if (window.attachEvent) {
+                        scriptTag.onreadystatechange = function () {
+                            var e = scriptTag.readyState;
+                            if (e === 'loaded' || e === 'complete') {
+                                scriptTag.onreadystatechange = null;
+                                fn();
+                            }
+                        };
+                    }
+					else {
+                        scriptTag.onload = fn;
+                    }
+                }
+                htmlTag.appendChild(scriptTag);
+            };
+            loadJs('http://changyan.sohu.com/upload/changyan.js', function () {
+                window.changyan.api.config({
+                    appid: appid,
+                    conf: conf
+                });
+            });
+        }
 	}
 	var i = t("zepto"),
 	s = t("customElement").create();
